78bdc2beca60431c023032c22a78f9c60849691f
SeanSibthorpe <seansg369@gmail.com>
2018-09-20 16:19:01 +0200

    StartScene, Rules

---
+++ 1900/Assets/Scripts/DeactivateAndActivate.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeactivateAndActivate : MonoBehaviour {
+
+    public GameObject deactivate;
+    public GameObject activate;
+
+    public void DeactivateActivate()
+    {
+        deactivate.SetActive(false);
+        activate.SetActive(true);
+    }
+}
